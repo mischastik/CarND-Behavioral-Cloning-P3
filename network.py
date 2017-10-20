4212474f6da35cf09e562872a99da59ffb612114
mischastik <michael.balda@gmail.com>
2017-10-20 23:30:11 +0200

    Improved model with additional convolutional layers

--- network.py
+++ network.py
@@ -52,7 +52,7 @@ validation_generator = generator(validation_samples, batch_size=32)
 
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 from keras.backend import tf as ktf
@@ -73,19 +73,25 @@ else:
     model.add(Lambda(lambda x: ktf.image.resize_images(x, (68, 200))))
     model.add(Convolution2D(3, 1, 1, border_mode='same', name='color_conv'))
     model.add(Convolution2D(24, (5, 5), activation="relu"))
+    model.add(Convolution2D(24, (5, 5), activation="relu", padding="same"))
     model.add(MaxPooling2D())
     model.add(Convolution2D(36, (5, 5), activation="relu"))
+    model.add(Convolution2D(36, (5, 5), activation="relu", padding="same"))
     model.add(MaxPooling2D())
     model.add(Convolution2D(48, (5, 5), activation="relu"))
+    model.add(Convolution2D(48, (5, 5), activation="relu", padding="same"))
     model.add(MaxPooling2D())
     model.add(Convolution2D(64, (3, 3), activation="relu"))
     model.add(Convolution2D(64, (3, 3), activation="relu", padding="valid"))
     model.add(Flatten())
     model.add(Dense(100))
+    #model.add(Dropout(0.5, noise_shape=None, seed=None))
     model.add(Dense(50))
+    #model.add(Dropout(0.5, noise_shape=None, seed=None))
     model.add(Dense(10))
+    #model.add(Dropout(0.5, noise_shape=None, seed=None))
     model.add(Dense(1))
     model.compile(loss='mse', optimizer='adam')
-    model.fit_generator(train_generator, steps_per_epoch=len(train_samples), epochs=4, validation_data=validation_generator,
+    model.fit_generator(train_generator, steps_per_epoch=len(train_samples), epochs=1, validation_data=validation_generator,
                     validation_steps=len(validation_samples))
     model.save('model.h5')
