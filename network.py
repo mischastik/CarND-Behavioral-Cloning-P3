6d79bd8f6ad393a3e34fbf34318fcd3913833f68
mischastik <michael.balda@gmail.com>
2017-10-22 22:53:31 +0200

    Added some comments

--- network.py
+++ network.py
@@ -16,6 +16,7 @@ train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
 def generator(samples, batch_size=32):
     num_samples = len(samples)
+    # we divide the batch_size by six to account for augmentations which effectively provide six samples for one entry
     batch_size = batch_size // 6
     while 1: # Loop forever so the generator never terminates
         shuffle(samples)
@@ -25,14 +26,18 @@ def generator(samples, batch_size=32):
             images = []
             measurements = []
             for batch_sample in batch_samples:
+                # load center, left and right image
                 for i in range(3):
                     source_path = batch_sample[i]
                     image = cv2.imread(source_path)
                     images.append(image)
+                # load the steering wheel angle
                 measurement = float(batch_sample[3])
                 measurements.append(measurement)
+                # apply static correction angles for left and right image
                 measurements.append(measurement + 0.2)
                 measurements.append(measurement - 0.2)
+            # augment the data-set with flipped images
             augmented_images, augmented_measurements = [], []
             for image, measurement in zip(images, measurements):
                 augmented_images.append(image)
@@ -60,6 +65,8 @@ from keras.models import load_model
 from pathlib import Path
 
 my_file = Path("model.h5")
+# if we already have a trained model, load it and continue training
+# important: Remember to delete model.h5 if the model architecture is changes, otherwise changes won't take effect
 if my_file.exists():
     model = load_model('model.h5', custom_objects={"ktf": ktf})
     model.fit_generator(train_generator, steps_per_epoch=len(train_samples), epochs=2, validation_data=validation_generator,
@@ -67,6 +74,8 @@ if my_file.exists():
     model.save('model.h5')
 
 else:
+    # we have no saved model, so build the model and train it, then save it.
+    # the model is inspired by the model suggested by NVidia with some additional convolutional layers.
     model = Sequential()
     model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
     model.add(Cropping2D(((65, 25), (0, 0))))
@@ -85,6 +94,7 @@ else:
     model.add(Convolution2D(64, (3, 3), activation="relu", padding="valid"))
     model.add(Flatten())
     model.add(Dense(100))
+    # dropout didn't help in this case, so we deactivated it
     #model.add(Dropout(0.5, noise_shape=None, seed=None))
     model.add(Dense(50))
     #model.add(Dropout(0.5, noise_shape=None, seed=None))
